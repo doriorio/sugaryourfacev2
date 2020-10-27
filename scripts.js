d57592975f7afaa9d38d1ee0dffe30cf70b85ffd
doriorio <itsdori@gmail.com>
2020-10-26 21:42:58 -0700

    working through styling

--- scripts.js
+++ scripts.js
@@ -1,5 +1,28 @@
-window.addEventListener('load', () => {
-    let body = document.getElementsByTagName('body')[0]
+document.addEventListener('DOMContentLoaded', () => {
+    var menuExpand = document.querySelectorAll(['.content-selector']);
+ 
+    
+    Array.prototype.forEach.call(menuExpand, menu => {
+        var expanderType = '.visually-hidden[data-type="';
+        if (menu.hasAttribute('data-type')) {
+            expanderType+= menu.getAttribute('data-type') + '"]' ;
+            console.log(expanderType);
+            var expander = document.querySelector([expanderType]);
+            console.log(expander);
+            let otherExpanders = document.getElementsByClassName('click-expand');
+            menu.addEventListener('click', function(){
+                expander.classList.toggle('visually-hidden');
+                Array.prototype.forEach.call(otherExpanders, otherItem => {
+                    if (otherItem != expander) {
+                        otherItem.classList.contains('visually-hidden') ? '' : otherItem.classList.add('visually-hidden'); 
 
-    //body.style.background = 'red';
+                    }
+                });
+            });
+        }
+        
+
+    });
+
+
 });
